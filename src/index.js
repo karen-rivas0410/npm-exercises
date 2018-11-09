8c84e59c809d192f1aa7ddbee0a7de8dffe7d6e9
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-11-08 18:55:54 -0600

    initial add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 "use strict";
 
-let sayHello = (input) => 'hello, ' + input;
+let sayHello = () => console.log('hello');
 
 sayHello();
+
+
+// index.js--REQUIRE METHOD!!--//
+const { niceMessage } = require('./say-hello');
+
+console.log(niceMessage()); // outputs from say-hello.js
+
+
+// index.js--  METHOD!!--//
+//
+// import say-Hello from './say-hello';
+//
+// console.log(say-hello); //
---
+++ src/say-hello.js
@@ -0,0 +1,5 @@
+//REQUIRE//
+
+const niceMessage = () => "It's Friday";
+
+module.exports = { niceMessage };   //needs to be in an object
